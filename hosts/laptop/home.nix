51bddbdb1154272245baa41a8218ebdab71dc9b9
Sw3d15h-F1s4 <samjt36@gmail.com>
2025-08-09 18:31:28 -0400

    add in old laptop changes
    rip laptop, you are going back to windows becuase teaching my family
    nixos would be painful

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -1,4 +1,12 @@
 { ... }:
 {
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "desc:Chimei Innolux Corporation 0x1521, 1920x1080@144, auto, 1"
+     ];
+  };
+
+
+  myHomeManager.hyprlock.default-monitor = "eDP-1";
 
 }
